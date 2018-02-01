496d2a5e6b9ee8585c067a8ebee984f5b0c3364b
Kirill <kirill.herz@gmail.com>
2018-02-01 23:48:41 +0300

    Вывод результата

--- Алгоритмы/Finding_loop_in_the list.py
+++ Алгоритмы/Finding_loop_in_the list.py
@@ -62,3 +62,14 @@ class List:
                 fast = fast.next
                 index += 1
         return index
+        
+
+l = List()
+l.add(0)
+l.add(1)
+l.add(2)
+l.add(3)
+l.add(4)
+l.add(5)
+l.addLoop(0,6)
+print(l.FindLoop())
