a1f32b20f9e153d47c1819895e7b9eebe8b1ea0b
Kirill <kirill.herz@gmail.com>
2018-10-19 18:08:55 +0300

    создана модель Person

--- LearningDjango/LearningDjango/LearningDjango/person/models.py
+++ LearningDjango/LearningDjango/LearningDjango/person/models.py
@@ -8,3 +8,11 @@ class City(models.Model):
         managed = False
         db_table = "city"
 
+class Person(models.Model):
+    id = models.IntegerField(primary_key = True)
+    name = models.CharField(max_length = 50)
+    city = models.ForeignKey(City, db_column = "id_city")
+    
+    class Meta:
+        managed = False
+        db_table = "person"
