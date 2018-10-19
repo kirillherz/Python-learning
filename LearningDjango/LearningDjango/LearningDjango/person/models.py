2cfdc29741fa4ede17dc50a884db37e5b8e9bc6f
Kirill <kirill.herz@gmail.com>
2018-10-19 18:08:46 +0300

    создана модель City

--- LearningDjango/LearningDjango/LearningDjango/person/models.py
+++ LearningDjango/LearningDjango/LearningDjango/person/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+    id = models.IntegerField(primary_key = True)
+    name = models.CharField(max_length = 50)
+    
+    class Meta:
+        managed = False
+        db_table = "city"
+
