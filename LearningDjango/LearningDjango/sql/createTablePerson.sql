e1e2642b58299dc080f0bbb3f21324b45edf5366
Kirill <kirill.herz@gmail.com>
2018-10-19 17:53:46 +0300

    создание новой таблицы

--- LearningDjango/LearningDjango/sql/createTablePerson.sql
+++ LearningDjango/LearningDjango/sql/createTablePerson.sql
@@ -1,6 +1,11 @@
-create table person(
+ CREATE TABLE IF NOT EXISTS person(
    id INTEGER PRIMARY KEY,
    name varchar(50) not null,
    id_city INT not null,
    foreign key(id_city) references city(id)
 );
+
+ CREATE TABLE IF NOT EXISTS city(
+    id INTEGER PRIMARY KEY,
+    name varchar(50) not null
+)
