b3a1dcc3813c787fed7196e4243809e126e37a75
Kirill <kirill.herz@gmail.com>
2018-10-20 10:49:15 +0300

    добавлен __str__ в City
    для корректного отображения в форме

--- LearningDjango/LearningDjango/LearningDjango/person/models.py
+++ LearningDjango/LearningDjango/LearningDjango/person/models.py
@@ -4,6 +4,9 @@ class City(models.Model):
     id = models.IntegerField(primary_key = True)
     name = models.CharField(max_length = 50)
     
+    def __str__(self):
+        return self.name
+
     class Meta:
         managed = False
         db_table = "city"
