a47e62314e5f61f8f7f8296dda8f3f7910d13a00
Kirill <kirill.herz@gmail.com>
2018-10-20 10:48:35 +0300

    исправлено представление

--- LearningDjango/LearningDjango/LearningDjango/person/views.py
+++ LearningDjango/LearningDjango/LearningDjango/person/views.py
@@ -3,6 +3,17 @@ from .models import *
 from .forms import *
 
 def index(request):
-    form = PersonForm()
+
+    if request.method == 'POST':
+        form = PersonForm(request.POST)
+        if form.is_valid():
+            form.save()
+            template = 'person/save.html'
+        else:
+           form = PersonForm()
+           template = 'person/index.html'
+    else:
+        template = 'person/index.html'
+        form = PersonForm() 
     context = {'form' : form}
-    return render(request, 'person/index.html', context)
+    return render(request, template, context)
